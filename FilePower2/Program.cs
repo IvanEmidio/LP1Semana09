72dc72a0e41c992fe1f5afb8d03c41a208eb9e96
Ivan <ivan.emidio@live.com.pt>
2024-05-10 12:00:29 +0100

    FilePower 2 Exercise

---
+++ FilePower2/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿using System;
+using System.IO;
+
+namespace FilePower2
+{
+    class Program
+    {
+        static void Main(string[] fileName)
+        {
+            Console.WriteLine("START");
+            string userInput = null;
+            string path = Path.Combine("FilePower2",fileName[0]);
+            using StreamWriter sw = new StreamWriter(path);
+
+            do
+            {
+                userInput = Console.ReadLine();
+                if (userInput != "")
+                    sw.WriteLine(userInput);
+            } while (userInput != ""); 
+        }
+    }
+}
